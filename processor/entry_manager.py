06aa785b511e59f4b9373482b5df6ee7d7ad5520
Evan <32987020+EvanBialo@users.noreply.github.com>
2018-03-15 21:23:34 -0400

    Created the find_entries function

--- processor/entry_manager.py
+++ processor/entry_manager.py
@@ -5,6 +5,25 @@ A list of all entries
 import pandas as pd
 
 
+def find_entries(df, match='', team='', name=''):
+    """
+    :param df: DataFrame which contains all entries
+    :param match: the match you want entries from
+    :param team: the team you want entries from
+    :param name: the name of the scouter for whom you want matches from (exact spelling)
+    :return: DataFrame which contains all of the entries which match all of the given parameters
+    """
+
+    if match != '':
+        df = df[df['Match'].isin(match)]
+
+    if team != '':
+        df = df[df['Team'].isin(team)]
+
+    if name != '':
+        df = df[df['Name'].isin(name)]
+
+
 class EntryManager:
     def __init__(self):
         self.entries = pd.DataFrame()
