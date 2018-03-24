a655fe0b2da5353a9c93383f28cd5d3e3e46f08d
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-03-23 20:09:24 -0400

    do delete test file

--- test.py
+++
@@ -1,7 +0,0 @@
-from processor import board, decoder
-bd = board.find("data/board", int("e3bb3f98", 16))
-a = list(decoder.decode("4001430b4d014e004f005000510452025300540555005602474b4a6a4a724776", bd))
-
-import pandas
-b = pandas.DataFrame(a)
-print(b)
