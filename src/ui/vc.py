b8d386928bd9262f12ee8ab2c6bdf0d47e6a10a8
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-04-08 13:43:27 -0400

    separate entry item class into different file

--- src/ui/vc.py
+++ src/ui/vc.py
@@ -5,35 +5,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
 
-
-class EntryInfoListItemWidget(QWidget):
-
-    def __init__(self, parent, entry_info):
-        super().__init__(parent, flags=Qt.Widget)
-
-        self.db_index = int(entry_info["Index"])
-
-        self.match = entry_info["Match"]
-        self.team = entry_info["Team"]
-        self.name = entry_info["Name"]
-        self.board_name = entry_info["Board"]
-
-        self.match_label = QLabel(str(self.match))
-        self.team_label = QLabel(str(self.team))
-        self.name_label = QLabel(str(self.name))
-        self.board_label = QLabel(str(self.board_name))
-
-        layout = QHBoxLayout()
-        layout.addWidget(self.match_label)
-        layout.addWidget(self.team_label)
-        layout.addWidget(self.name_label)
-        layout.addWidget(self.board_label)
-
-        self.setLayout(layout)
-
-        self.setMinimumSize(200, 30)
-        self.resize(200, 30)
-        self.show()
+from src.ui.entryitem import EntryInfoListItemWidget
 
 
 class VerificationCenter(QMainWindow):
