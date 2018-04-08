277f4d3d94a44b423391e88c0d7d9bd883ae27b4
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-04-08 10:33:21 -0400

    update verification.py with newer version

--- src/ui/verification.py
+++ src/ui/verification.py
@@ -6,13 +6,21 @@ from PyQt5.QtWidgets import *
 
 
 class EntryInfoListItemWidget(QWidget):
+
     def __init__(self, parent, entry_info):
         super().__init__(parent, flags=Qt.Widget)
 
-        self.match_label = QLabel(str(entry_info["Match"]))
-        self.team_label = QLabel(str(entry_info["Team"]))
-        self.name_label = QLabel(str(entry_info["Name"]))
-        self.board_label = QLabel(str(entry_info["Board"]))
+        self.db_index = int(entry_info["Index"])
+
+        self.match = entry_info["Match"]
+        self.team = entry_info["Team"]
+        self.name = entry_info["Name"]
+        self.board_name = entry_info["Board"]
+
+        self.match_label = QLabel(str(self.match))
+        self.team_label = QLabel(str(self.team))
+        self.name_label = QLabel(str(self.name))
+        self.board_label = QLabel(str(self.board_name))
 
         layout = QHBoxLayout()
         layout.addWidget(self.match_label)
@@ -32,7 +40,7 @@ class TestAppWindow(QMainWindow):
     def __init__(self):
         super().__init__(flags=Qt.Window)
 
-        self.setup_entries_table()
+        self.setup_entries_list()
         self.setup_menus()
 
         self.resize(500, 500)
@@ -40,13 +48,19 @@ class TestAppWindow(QMainWindow):
         self.setWindowTitle("Verification Center")
         self.show()
 
-    def setup_entries_table(self):
+    def setup_entries_list(self):
+
         import random
         entries = QListWidget(self)
+
+        def on_entry_clicked(item: QListWidgetItem):
+            _entry_info = entries.itemWidget(item)
+            self.statusBar().showMessage(str(_entry_info.team))
 
         for i in range(100):
             entry_info = EntryInfoListItemWidget(entries, {"Match": i // 6 + 1,
                                                            "Team": random.randint(1, 7999),
+                                                           "Index": random.randint(1, 660),
                                                            "Board": random.choice(["Red 1",
                                                                                    "Red 2",
                                                                                    "Red 3",
@@ -62,29 +76,9 @@ class TestAppWindow(QMainWindow):
         entries.move(0, 30)
 
         entries.setFixedWidth(300)
-        entries.setFixedHeight(475)
+        entries.setFixedHeight(400)
 
-    def create_menu_action(self, name, callback=None, shortcut=None, role=None):
-        """
-        :param name: The lable of the menu
-        :param callback: Event handler
-        :param shortcut: Keyboard shortcut
-        :param role: Menu Role (for Mac OS)
-        :return: A QAction that includes the callback
-        """
-
-        action = QAction(name, self)
-
-        if callback is not None:
-            action.triggered.connect(callback)
-
-        if shortcut is not None:
-            action.setShortcut(shortcut)
-
-        if role is not None:
-            action.setMenuRole(role)
-
-        return action
+        entries.itemClicked.connect(on_entry_clicked)
 
     def setup_menus(self):
         """
@@ -92,15 +86,43 @@ class TestAppWindow(QMainWindow):
         :return:
         """
 
+        def create_menu_action(name, callback=None, shortcut=None, role=None):
+            """
+            :param name: The lable of the menu
+            :param callback: Event handler
+            :param shortcut: Keyboard shortcut
+            :param role: Menu Role (for Mac OS)
+            :return: A QAction that includes the callback
+            """
+
+            action = QAction(name, self)
+
+            if callback is not None:
+                action.triggered.connect(callback)
+
+            if shortcut is not None:
+                action.setShortcut(shortcut)
+
+            if role is not None:
+                action.setMenuRole(role)
+
+            return action
+
+        def on_open_menu_triggered():
+            file_name = QFileDialog.getOpenFileName(self, "Open Database", filter="(*.warp7)")
+
+        def on_import_csv_menu_triggered():
+            file_name = QFileDialog.getExistingDirectory(self, "Open Folder", "", QFileDialog.ShowDirsOnly)
+
         menus = {
             "File": [
-                ["Open", self.on_open_menu_triggered, Qt.CTRL | Qt.Key_O],
+                ["Open", on_open_menu_triggered, Qt.CTRL | Qt.Key_O],
                 ["Save", None, Qt.CTRL | Qt.Key_S],
                 ["Save As", None, Qt.CTRL | Qt.SHIFT | Qt.Key_S],
                 ["Close", self.close, Qt.CTRL | Qt.Key_W]
             ],
             "Import": [
-                ["From CSV scans", self.on_import_csv_menu_triggered, Qt.CTRL | Qt.Key_I]
+                ["From CSV scans", on_import_csv_menu_triggered, Qt.CTRL | Qt.Key_I]
             ]
 
         }
@@ -111,19 +133,14 @@ class TestAppWindow(QMainWindow):
             menu_view = QMenu(menu_name, self)
 
             for menu_item in menus[menu_name]:
-                menu_action = self.create_menu_action(*menu_item)
+                menu_action = create_menu_action(*menu_item)
                 print(menu_action)
                 menu_view.addAction(menu_action)
 
             menu_bar.addMenu(menu_view)
 
-    def on_open_menu_triggered(self):
-        file_name = QFileDialog.getOpenFileName(self, "Open Database", filter="(*.warp7)")
-        self.statusBar().showMessage(file_name[0])
-
-    def on_import_csv_menu_triggered(self):
-        file_name = QFileDialog.getExistingDirectory(self, "Open Folder", "", QFileDialog.ShowDirsOnly)
-        self.statusBar().showMessage(file_name[0])
+    def on_entries_list_selected(self, o):
+        self.statusBar().showMessage("hi")
 
 
 if __name__ == '__main__':
