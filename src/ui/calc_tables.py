10009a7b2eb74947e3d6f2dbda67647ae07abcc7
LemkeBen <29043943+LemkeBen@users.noreply.github.com>
2018-04-18 15:10:34 -0400

    Fix Evan's latest push

--- src/ui/calc_tables.py
+++ src/ui/calc_tables.py
@@ -1,9 +1,6 @@
-from src.model.vcmanager import VerificationManager
 from src.ui.tablesui import Interface
-
 
 class CalculationReader(Interface):
     def __init__(self, db_path, csv_dir_path, board_dir_path):
-        self.manager = VerificationManager(db_path, csv_dir_path, board_dir_path)
         super().__init__()
         pass
