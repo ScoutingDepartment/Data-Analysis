6e380e1fec4cebf454d62ef9383824a3847e818f
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-04-25 10:05:13 -0400

    add app cycles

--- src/ui/analysis/analysis_center.py
+++ src/ui/analysis/analysis_center.py
@@ -20,11 +20,15 @@ class AnalysisCenter(AnalysisUI):
         # self.table_content.update_contents("Raw Data", self.manager["raw_data"].data)
 
     def on_table_nav_selected(self):
-        selected = self.tables_nav.selectedItems()
-        if selected:
-            new_table_name = self.manager.title_to_name(selected[0].text())
-            new_table = self.manager[new_table_name]
-            self.table_content.update_contents(new_table.title, new_table.data)
+        try:
+            selected = self.tables_nav.selectedItems()
+            if selected:
+                new_table_name = self.manager.title_to_name(selected[0].text())
+                new_table = self.manager[new_table_name]
+                self.table_content.update_contents(new_table.title, new_table.data)
+        except:
+            import traceback
+            traceback.print_exc()
 
     def on_calculate_with_tba(self):
         self.manager.compute_all(tba_available=True)
