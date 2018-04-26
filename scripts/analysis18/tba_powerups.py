11e618c4ddf2e47f1bd5335e737f95dc70ed71f3
yuliu2016 <yuliu2016@users.noreply.github.com>
2018-04-25 23:38:44 -0400

    finish tba powerups

--- scripts/analysis18/tba_powerups.py
+++ scripts/analysis18/tba_powerups.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import tbapy
 
 TITLE_NAME = "Tba powerups"
 SOURCE_NAME = "tba_powerups"
@@ -12,21 +11,39 @@ LABELS = ["Match",
           "Blue boost played", "Blue boost total"]
 
 
-def row_data_generator(manager, tba):
+def row_data_generator(manager):
+    tba = manager.tba
     event = tba.event_matches(manager.tba_event)
     for match in event:
+        if match['score_breakdown'] != None and match['comp_level'] == 'qm':
+            row_data = {'Match': match['match_number']}
+            for alliance in ['red', 'blue']:
+                row_data[alliance.capitalize() + " force played"] = match['score_breakdown'][alliance][
+                    'vaultForcePlayed']
+                row_data[alliance.capitalize() + " force total"] = match['score_breakdown'][alliance]['vaultForceTotal']
+                row_data[alliance.capitalize() + " levitate played"] = match['score_breakdown'][alliance][
+                    'vaultLevitatePlayed']
+                row_data[alliance.capitalize() + " levitate total"] = match['score_breakdown'][alliance][
+                    'vaultLevitateTotal']
+                row_data[alliance.capitalize() + " boost played"] = match['score_breakdown'][alliance][
+                    'vaultBoostPlayed']
+                row_data[alliance.capitalize() + " boost total"] = match['score_breakdown'][alliance]['vaultBoostTotal']
+            yield row_data
 
-        row_data = {'Match': match}
-        for alliance in ['Red', 'Blue']:
-            row_data[alliance + " force played"] = match['score_breakdown'][alliance]['vaultForcePlayed']
-            row_data[alliance + " force total"] = match['score_breakdown'][alliance]['vaultForceTotal']
-            row_data[alliance + " levitate played"] = match['score_breakdown'][alliance]['vaultLevitatePlayed']
-            row_data[alliance + " levitate total"] = match['score_breakdown'][alliance]['vaultLevitateTotal']
-            row_data[alliance + " boost played"] = match['score_breakdown'][alliance]['vaultBoostPlayed']
-            row_data[alliance + " boost total"] = match['score_breakdown'][alliance]['vaultBoostTotal']
-        yield row_data
+    yield {"Match": 0,
+           "Red force played": 0,
+           "Red force total": 0,
+           "Red levitate played": 0,
+           "Red levitate total": 0,
+           "Red boost played": 0,
+           "Red boost total": 0,
+           "Blue force played": 0,
+           "Blue force total": 0,
+           "Blue levitate played": 0,
+           "Blue levitate total": 0,
+           "Blue boost played": 0,
+           "Blue boost total": 0}
 
 
 def compute_table(manager):
-    tba = tbapy.TBA(manager.tba_key)
-    return pd.DataFrame(row_data_generator(manager, tba))[LABELS]
+    return pd.DataFrame(row_data_generator(manager))[LABELS]
