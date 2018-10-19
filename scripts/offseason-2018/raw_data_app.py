c079c116305218a9437e52806aab9bf672e909f9
yuliu2016 <22801773+yuliu2016@users.noreply.github.com>
2018-10-19 06:33:26 -0400

    add data from endgame

--- scripts/offseason-2018/raw_data_app.py
+++ scripts/offseason-2018/raw_data_app.py
@@ -19,16 +19,25 @@ LABELS = ["Team Number",
           "Scale",
           "Times Cube Dropped",
           "Defense Time",
-          "Endgame Type"]
+          "Endgame Type"
+          "Total Platform Duration",
+          "Last Platform Duration",
+          "Platform Attempts",
+          "Total Climbed Duration",
+          "Last Climbed Duration",
+          "Climb Attempts",
+          "Objective",
+          "Comments"
+          ]
 
-START_POSITION = [
+START_POSITIONS = [
     "None",
     "Left",
     "Center",
     "Right"
 ]
 
-ENDGAME_TYPE = [
+ENDGAME_TYPES = [
     "None",
     "Platform",
     "Failed Climb",
@@ -36,6 +45,16 @@ ENDGAME_TYPE = [
     "Double Climb",
     "Single Climb, Lifting Another Robot",
     "Lifted by Another Robot"
+]
+
+OBJECTIVES = [
+    "Select",
+    "Scale",
+    "Alliance Switch",
+    "Opponent Switch",
+    "Exchange",
+    "Defense",
+    "Support"
 ]
 
 
@@ -54,19 +73,66 @@ def row_data_generator(manager):
                     else:
                         defense_pairs[int((index - 1) / 2)].append(value)
                     start = not start
-                if len(defense_pairs[-1]) == 1:
-                    defense_pairs[-1].append(150)
-                defence_values = []
-                for i in defense_pairs:
-                    defence_values.append(i[1] - i[0])
-                defense_time = sum(defence_values)
+                if defense_pairs:
+                    if len(defense_pairs[-1]) == 1:
+                        defense_pairs[-1].append(150)
+                    defence_values = []
+                    for i in defense_pairs:
+                        defence_values.append(i[1] - i[0])
+                    defense_time = sum(defence_values)
+                else:
+                    defense_time = 0
+
+            platform_presses = entry.look("Platform")
+            platform_pairs = []
+            start = True
+
+            for index, value in enumerate(platform_presses):
+                if start:
+                    platform_pairs.append([value])
+                else:
+                    platform_pairs[int((index - 1) / 2)].append(value)
+                start = not start
+
+            platform_attempts = len(platform_pairs)
+
+            if platform_pairs and len(platform_pairs[-1]) == 1:
+                platform_pairs[-1].append(150)
+                last_platform_duration = 150 - platform_pairs[-1][0]
+                platform_attempts -= 1
+            else:
+                last_platform_duration = 0
+
+            total_platform_duration = sum(map(lambda x: x[1] - x[0], platform_pairs))
+
+            climbed_presses = entry.look("Climbed")
+            climbed_pairs = []
+            start = True
+
+            for index, value in enumerate(climbed_presses):
+                if start:
+                    climbed_pairs.append([value])
+                else:
+                    climbed_pairs[int((index - 1) / 2)].append(value)
+                start = not start
+
+            climb_attempts = len(climbed_pairs)
+
+            if climbed_pairs and len(climbed_pairs[-1]) == 1:
+                climbed_pairs[-1].append(150)
+                last_climbed_duration = 150 - climbed_pairs[-1][0]
+                climb_attempts -= 1
+            else:
+                last_climbed_duration = 0  # No climb
+
+            total_climbed_duration = sum(map(lambda x: x[1] - x[0], climbed_pairs))
 
             row_data = {
                 "Team Number": entry.team,
                 "Alliance": entry.board.alliance(),
                 "Match Number": entry.match,
 
-                "Start Position": START_POSITION[entry.final_value("Start position", 0)],
+                "Start Position": START_POSITIONS[entry.final_value("Start position", 0)],
                 "Auto Line": entry.final_value("Auto line", default=0),
 
                 "Exchange Auto Successes": entry.count("Auto exchange"),
@@ -89,7 +155,18 @@ def row_data_generator(manager):
                                        entry.count("Tele scale")),
 
                 "Defense Time": defense_time,
-                "Endgame Type": ENDGAME_TYPE[entry.final_value("Endgame type", 0)]
+                "Endgame Type": ENDGAME_TYPES[entry.final_value("Endgame type", 0)],
+
+                "Total Platform Duration": total_platform_duration,
+                "Last Platform Duration": last_platform_duration,
+                "Platform Attempts": platform_attempts,
+
+                "Total Climbed Duration": total_climbed_duration,
+                "Last Climbed Duration": last_climbed_duration,
+                "Climb Attempts": climb_attempts,
+
+                "Objective": OBJECTIVES[entry.final_value("Objective", 0)],
+                "Comments": entry.comments
             }
 
             # Fix times cube dropped
